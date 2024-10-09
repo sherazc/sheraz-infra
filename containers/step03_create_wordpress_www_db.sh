8943a563e814d9cd3e9841e12915102edb80e902
sheraz <sheraz@mbp16.com>
2024-10-08 23:10:49 -0400

    Setup INFRA

--- containers/step03_create_wordpress_www_db.sh
+++ containers/step03_create_wordpress_www_db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MYSQL_PWD="password"
+export SHERAZ_INFRA_DB_ROOT_PASSWORD="password"
 export WORDPRESS_WWW_USER_PASSWORD="password"
 
 # Variables for MySQL credentials, container name, and database/user details
@@ -9,7 +9,7 @@ WORDPRESS_WWW_DB_NAME="wordpresswwwdb"
 WORDPRESS_WWW_USER="wordpresswwwuser"
 
 # Execute SQL commands inside the container
-docker exec -e MYSQL_PWD=$MYSQL_ROOT_PASSWORD -i $SHERAZ_INFRA_DB_CONTAINER_NAME mysql -u root <<EOF
+docker exec -e MYSQL_PWD=$SHERAZ_INFRA_DB_ROOT_PASSWORD -i $SHERAZ_INFRA_DB_CONTAINER_NAME mysql -u root <<EOF
 CREATE DATABASE $WORDPRESS_WWW_DB_NAME;
 CREATE USER '$WORDPRESS_WWW_USER'@'%' IDENTIFIED BY '$WORDPRESS_WWW_USER_PASSWORD';
 GRANT ALL PRIVILEGES ON $WORDPRESS_WWW_DB_NAME.* TO '$WORDPRESS_WWW_USER'@'%';
