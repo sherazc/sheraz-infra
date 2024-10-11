bd7b699e52c6a2acf926f318d6cf9fc97b080d38
sheraz <sheraz@mbp16.com>
2024-10-11 01:28:02 -0400

    TODO for event-register app installation.

---
+++ containers/step06_create_app_event_container.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# in production environment install java and put the commands in system-setup.sh
+
+# In event-register
+# create a startup script file
+# Create a event-register.service file
+# put the startup script file in .service file
+
+# In this file
+# scp below files
+# - /etc/systemd/system/event-register.service
+# - /opt/sheraz-infra/event-register/event-register.sh
+# - /opt/sheraz-infra/event-register/event-register.jar
