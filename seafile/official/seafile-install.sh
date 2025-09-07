8e1a73160dc03863401f8759d257c4e4beee8fd2
sheraz@mbp16 <sheraz@bitsegment.com>
2025-09-07 01:52:24 -0400

    official

--- seafile/official/seafile-install.sh
+++ seafile/official/seafile-install.sh
@@ -4,6 +4,6 @@ docker-compose -p seafile \
   -f seafile-server.yml \
   up -d
 
-docker-compose -p seafile-doc \
-  -f seadoc.yml \
-  up -d
+# docker-compose -p seafile-doc \
+#   -f seadoc.yml \
+#   up -d
---
+++ seafile/official/seafile-uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker-compose -p seafile -f seafile-server.yml down -v
+
+rm -rf seafile-data
+rm -rf seafile-mysql
+rm -rf seadoc-data
+
