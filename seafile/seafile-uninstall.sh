b518eb40d1c3d524a37aa3aa93a59712b131c3ae
sheraz@mbp16 <sheraz@bitsegment.com>
2025-09-07 08:20:35 -0400

    seafile uninstall

--- seafile/seafile-uninstall.sh
+++ seafile/seafile-uninstall.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+source .env
 docker-compose -p seafile -f seafile-server.yml down -v
 
-rm -rf seafile-data
-rm -rf seafile-mysql
+rm -rf ${SEAFILE_VOLUME}
+rm -rf ${SEAFILE_MYSQL_VOLUME}
+rm -rf ${SEAFILE_CADDY_VOLUME}
+rm -rf ${SEADOC_VOLUME}
